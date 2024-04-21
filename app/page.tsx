7d75c3709306bf24dac1174a5a8b131b92608dea
Nathaly <natharb09@gmail.com>
2024-04-20 21:00:22 -0500

    OB-11 Implementing the server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,15 @@
+import { getFormData } from '@/actions/_form';
 import { RegistrationForm } from '@/components/form/form';
 import { Steps } from '@/components/form/steps';
-import { formData1 } from '@/lib/mock-data';
 
-export default function Home() {
-  const steps = [
-    {
-      id: 1,
-      name: 'Información de la Víctima',
-      fields: formData1,
-    },
-    {
-      id: 2,
-      name: 'Información 2',
-      fields: formData1,
-    },
-    {
-      id: 3,
-      name: 'Información 3',
-      fields: formData1,
-    },
-    {
-      id: 4,
-      name: 'Información 4',
-      fields: formData1,
-    },
-  ]
+export default async function Home() {
+  const multiStepFormData = await getFormData();
+  console.log('Home multiStepFormData ::: ', multiStepFormData);
 
   return (
     <section className="m-4 flex flex-col gap-10 divide-y divide-gray-300 rounded-md border border-gray-300 p-6 md:divide-y-0">
-      <Steps steps={steps} />
-      <RegistrationForm steps={steps} />
+      <Steps steps={multiStepFormData} />
+      <RegistrationForm steps={multiStepFormData} />
     </section>
   );
 }
