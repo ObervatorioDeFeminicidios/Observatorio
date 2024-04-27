ab4575d5fc158bca04c8b32ebf3f47fef39d863c
Nathaly <natharb09@gmail.com>
2024-04-27 12:30:59 -0500

    OB-17 Fixing the UI of the select field

--- components/form/field/select.tsx
+++ components/form/field/select.tsx
@@ -38,70 +38,68 @@ export const FieldSelect = ({ formField, form }: FieldProps) => {
       control={form.control}
       name={formField.id}
       render={({ field }) => (
-        <FormItem className="flex items-baseline justify-between">
-          <FormLabel className="max-w-[40%]">{formField.label}</FormLabel>
-          <div className="flex flex-col gap-2">
-            <Popover open={open} onOpenChange={setOpen}>
-              <PopoverTrigger asChild>
-                <FormControl>
-                  <Button
-                    variant="outline"
-                    role="combobox"
-                    className={cn(
-                      '!m-0 w-[300px] justify-between',
-                      !field.value && 'text-muted-foreground',
-                    )}
-                    aria-expanded={open}
-                  >
-                    {field.value
-                      ? (formField as SelectField).options.find(
-                          (option) => option.label === field.value,
-                        )?.label
-                      : 'Select option...'}
-                    <ChevronUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                  </Button>
-                </FormControl>
-              </PopoverTrigger>
-              <PopoverContent className="w-[300px] p-0">
-                <Command>
-                  <CommandInput placeholder="Buscar..." className="h-9" />
-                  <CommandEmpty>
-                    <SelectEmpty
-                      fieldId={formField.id}
-                      closePopover={() => setOpen(false)}
-                    />
-                  </CommandEmpty>
-                  <CommandGroup>
-                    <ScrollArea className="max-h-72 w-auto overflow-y-auto">
-                      {(formField as SelectField).options.map((option) => (
-                        <CommandItem
-                          key={option.value}
-                          value={option.label}
-                          onSelect={() => {
-                            form.setValue(formField.id, option.label);
-                            form.setValue(`cod_${formField.id}`, option.value);
-                            console.log(form.getValues());
-                            setOpen(false);
-                          }}
-                        >
-                          {option.label}
-                          <CheckIcon
-                            className={cn(
-                              'ml-auto h-4 w-4',
-                              option.label === field.value
-                                ? 'opacity-100'
-                                : 'opacity-0',
-                            )}
-                          />
-                        </CommandItem>
-                      ))}
-                    </ScrollArea>
-                  </CommandGroup>
-                </Command>
-              </PopoverContent>
-            </Popover>
-            <FormMessage />
-          </div>
+        <FormItem className="flex flex-col">
+          <FormLabel>{formField.label}</FormLabel>
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <FormControl>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  className={cn(
+                    'justify-between',
+                    !field.value && 'text-muted-foreground',
+                  )}
+                  aria-expanded={open}
+                >
+                  {field.value
+                    ? (formField as SelectField).options.find(
+                        (option) => option.label === field.value,
+                      )?.label
+                    : 'Select option...'}
+                  <ChevronUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-[300px] p-0">
+              <Command>
+                <CommandInput placeholder="Buscar..." className="h-9" />
+                <CommandEmpty>
+                  <SelectEmpty
+                    fieldId={formField.id}
+                    closePopover={() => setOpen(false)}
+                  />
+                </CommandEmpty>
+                <CommandGroup>
+                  <ScrollArea className="max-h-72 w-auto overflow-y-auto">
+                    {(formField as SelectField).options.map((option) => (
+                      <CommandItem
+                        key={option.value}
+                        value={option.label}
+                        onSelect={() => {
+                          form.setValue(formField.id, option.label);
+                          form.setValue(`cod_${formField.id}`, option.value);
+                          console.log(form.getValues());
+                          setOpen(false);
+                        }}
+                      >
+                        {option.label}
+                        <CheckIcon
+                          className={cn(
+                            'ml-auto h-4 w-4',
+                            option.label === field.value
+                              ? 'opacity-100'
+                              : 'opacity-0',
+                          )}
+                        />
+                      </CommandItem>
+                    ))}
+                  </ScrollArea>
+                </CommandGroup>
+              </Command>
+            </PopoverContent>
+          </Popover>
+          <FormMessage />
         </FormItem>
       )}
     />
