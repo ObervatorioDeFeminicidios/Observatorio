0edd66560b688102d4e0b05ce32a83daab8fda68
Nathaly <natharb09@gmail.com>
2024-09-24 18:18:43 +0200

    OB-47 Centralizing the api routes

--- auth.config.ts
+++ auth.config.ts
@@ -1,20 +1,21 @@
 import type { NextAuthConfig } from 'next-auth';
+import { API_ROUTES } from './app/api';
 
 export const authConfig: NextAuthConfig = {
   pages: {
-    signIn: '/login',
+    signIn: API_ROUTES.login,
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       // Protecting the path
-      const isOnRegistration = nextUrl.pathname.startsWith('/dashboard');
+      const isOnRegistration = nextUrl.pathname.startsWith(API_ROUTES.dashboard);
       if (isOnRegistration) {
         if (isLoggedIn) return true;
         // Redirect unauthenticated users to login page
         return false;
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard/registration', nextUrl));
+        return Response.redirect(new URL(API_ROUTES.registration, nextUrl));
       }
       return true;
     },
