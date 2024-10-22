bbae27b9fa72e56e843a9d8c37ca2bc0f6bab96f
Nathaly <natharb09@gmail.com>
2024-10-22 21:31:21 +0100

    OB-65 Setting the register data as default values for the form

--- components/form/form.tsx
+++ components/form/form.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { getRegisterData } from '@/actions/_form';
+import { fetchRegister } from '@/actions/_form';
 import { Form } from '@/components/ui/form';
 import { getDefaultValues, getSchema, getSchemaByStep } from '@/lib/form';
 import { useFormStore } from '@/store/registration-form';
-import { Step } from '@/types';
+import { OptionField, Step } from '@/types';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useParams } from 'next/navigation';
 import React from 'react';
@@ -12,6 +12,7 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { FormStep } from './form-step';
 import { Navigation } from './navigation';
+import { useQuery } from '@tanstack/react-query';
 
 type RegistrationFormProps = {
   steps: Step[];
@@ -21,47 +22,35 @@ export const RegistrationForm = ({ steps }: RegistrationFormProps) => {
   const params = useParams<{ id: string }>();
   const isEditMode = !!params?.id;
 
-  const { currentStep, updateFormSchemas } = useFormStore();
+  const { currentStep, updateFormSchemas, updateInitialAssociatedViolences } = useFormStore();
   const formSchema = React.useMemo(() => getSchema(steps), [steps]);
   const formRef = React.useRef<HTMLFormElement>(null);
   const totalSteps = steps.length;
 
-  // State to handle loading state
-  const [isLoading, setIsLoading] = React.useState(isEditMode);
+  // Fetching the register
+  const dataQuery = useQuery({
+    queryKey: ['data', params.id],
+    queryFn: () => fetchRegister(params.id),
+    enabled: isEditMode,
+  });
 
   // Initialize form with zod resolver and default values
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: getDefaultValues(steps),
+    defaultValues: !isEditMode ? getDefaultValues(steps) : undefined,
   });
 
   const { reset } = form;
 
-  // Fetch register data for edit mode (if params.id exists)
+  // Handle form schemas and reset form values once data is fetched
   React.useEffect(() => {
-    const fetchRegisterData = async () => {
-      if (isEditMode) {
-        try {
-          const response = await getRegisterData(params.id);
-          console.log('Form response ::: ', response);
-          if (response.success) {
-            // Reset the form with the fetched data
-            reset(response.results);
-          } else {
-            console.error('Error fetching register data:', response.errors);
-          }
-        } catch (error) {
-          console.error('Failed to fetch register data:', error);
-        } finally {
-          setIsLoading(false);
-        }
-      }
-    };
+    if (dataQuery.data?.results) {
+      reset(dataQuery.data.results); // Reset form values with fetched data
+      updateInitialAssociatedViolences(dataQuery.data.results.violencia_asociada as OptionField[]);
+    }
+  }, [dataQuery.data, reset, updateInitialAssociatedViolences]);
 
-    fetchRegisterData();
-  }, [params?.id, reset]);
-
-  // Getting the multistep form schema by step
+  // Updating the multistep form schema by step and default data
   React.useEffect(() => {
     updateFormSchemas({
       firstSchema: getSchemaByStep(steps[0].fields),
@@ -69,10 +58,10 @@ export const RegistrationForm = ({ steps }: RegistrationFormProps) => {
       thirdSchema: getSchemaByStep(steps[2].fields),
       fourthSchema: getSchemaByStep(steps[3].fields),
     });
-  }, []);
+  }, [steps, updateFormSchemas]);
 
   // TODO: Implement skeleton
-  if (isLoading) {
+  if (dataQuery.isLoading) {
     return <div>Loading...</div>;
   }
 
