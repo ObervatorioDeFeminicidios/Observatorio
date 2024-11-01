7a8a96dd8530123314481e3d5565d9553c1b7b88
Nathaly <natharb09@gmail.com>
2024-11-01 15:44:12 +0000

    OB-67 Showing text area in the four step for some fields

--- components/form/field/input.tsx
+++ components/form/field/input.tsx
@@ -6,10 +6,17 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { cn } from '@/lib/utils';
 import { FieldProps } from '@/types';
 import { ControllerRenderProps, FieldValues } from 'react-hook-form';
 
-export const FieldInput = ({ formField, form }: FieldProps) => {
+export const FieldInput = ({
+  stepIndex,
+  formIndex,
+  formField,
+  form,
+}: FieldProps) => {
   const handleOnChange = (
     e: React.ChangeEvent<HTMLInputElement>,
     field: ControllerRenderProps<FieldValues, string>,
@@ -23,22 +30,35 @@ export const FieldInput = ({ formField, form }: FieldProps) => {
     }
   };
 
+  const isStepFourField = stepIndex === 4 && (formIndex as number) > 3;
+
+  const itemClassName = cn({
+    'col-span-2': isStepFourField,
+  });
+
   return (
     <FormField
       control={form.control}
       name={formField.id}
       render={({ field }) => (
-        <FormItem>
+        <FormItem className={itemClassName}>
           <FormLabel className="text-secondary-foreground">
             {formField.label}
           </FormLabel>
           <FormControl>
-            <Input
-              {...field}
-              className="font-light"
-              onChange={(e) => handleOnChange(e, field)}
-              type={formField.type !== 'int' ? formField.type : 'number'}
-            />
+            {!isStepFourField ? (
+              <Input
+                {...field}
+                className="font-light"
+                onChange={(e) => handleOnChange(e, field)}
+                type={formField.type !== 'int' ? formField.type : 'number'}
+              />
+            ) : (
+              <Textarea
+                {...field}
+                className="h-32 w-full resize-y overflow-auto break-words text-left"
+              />
+            )}
           </FormControl>
           <FormMessage />
         </FormItem>
